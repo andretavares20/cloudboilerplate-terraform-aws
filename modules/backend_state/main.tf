1b623d6d398a4ea1e091628dba8fc952996842b3
Andre Tavares <andretavares16@gmail.com>
2025-07-28 21:04:36 -0300

    atualizações

--- modules/backend_state/main.tf
+++ modules/backend_state/main.tf
@@ -1,22 +1,26 @@
-resource "aws_s3_bucket" "tf_state" {
-  bucket = var.bucket_name
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = {
-    Name = "${var.project}-state"
-  }
+provider "aws" {
+  region = "us-east-1"
 }
+
+# resource "aws_s3_bucket" "tf_state" {
+#   bucket = var.bucket_name
+
+#   versioning {
+#     enabled = true
+#   }
+
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+
+#   tags = {
+#     Name = "${var.project}-state"
+#   }
+# }
 
 resource "aws_dynamodb_table" "tf_lock" {
   name         = var.dynamodb_table
